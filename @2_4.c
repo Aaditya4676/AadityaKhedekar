3ac3ea76db4cdbe8614083fea6ee6eb33b498d94
AadityaKhedekar <54372809+AadityaKhedekar@users.noreply.github.com>
2019-09-09 04:05:21 +0530

    Add files via upload

--- @2_4.c
+++ @2_4.c
@@ -12,22 +12,22 @@
     printf("Enter the value of N \n");
     scanf("%d", &n);
     printf("Enter %d real numbers \n", n);
-    for (i = 1; i < n; i++)
+    for (i = 1; i < n+1; i++)
     {
         scanf("%f", &x[i]);
     }
     /*  Compute the sum of all elements */
-    for (i = 1; i < n; i++)
+    for (i = 1; i < n+1; i++)
     {
-        sum = sum + x[i];
+        sum = sum + (x[i] -6);
     }
     average = sum / (float)n;
     /*  Compute  variance   */
     for (i = 0; i < n; i++)
     {
-        sum1 = sum1 + pow((x[i] - average), 2);
+        sum1 = sum1 + pow(((x[i] - 6) - average), 2);
     }
-    variance = sum1 / (float)n;
+    variance = (sum1 / (float)n) -1 ;
     printf("Average of all elements = %.2f\n", average);
     printf("variance of all elements = %.2f\n", variance);
 }
